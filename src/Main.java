67252fea31fcde51aca07dab5200a158476d3ff9
Timymur <hitechmag2013@mail.ru>
2024-11-27 16:29:44 +0500

    Calc

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите выражение (пример 2 + 3): ");
+        String input = scanner.nextLine();
+        String answer = calc(input);
+        System.out.println(answer);
+    }
+
+    public static String calc(String input) {
+
+        String[] mass = input.split(" ");
+        if (input.length() != 3) return "Неверная запись";
+
+        try {
+
+            int x = Integer.parseInt(mass[0]);
+            int y = Integer.parseInt(mass[2]);
+            int answer;
+
+            if (    x > 10 || x < 1 ||
+                    y > 10 || y < 1)
+                return "Значения не входят в рабочий диапазон от 1 до 10";
+
+            switch (mass[1]) {
+                case ("+"):
+                    answer = x + y;
+                    break;
+                case ("-"):
+                    answer = x - y;
+                    break;
+                case ("*"):
+                    answer = x * y;
+                    break;
+                case ("/"):
+                    answer = x / y;
+                    break;
+                default:
+                    return "Калькулятор не знает такой операции";
+            }
+
+            return Integer.toString(answer);
+        }
+
+        catch (NumberFormatException e) {
+            return "Ошибка: введите корректные числа";
+        }
+
+        catch (Exception e) {
+            return "Произошла ошибка: " + e.getMessage();
         }
     }
 }
